5922fa5dcd98efd351041e50ca34f0cccf85d228
lihao <dahao@qq.com>
2021-05-20 11:04:14 +0800

    init webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+
+module.exports = {
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "index.bundle.js"
+    },
+    devServer: {
+        port: 3000,
+        watchContentBase: true
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {loader: "babel-loader"}
+        },
+        {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"]
+        }]
+    },
+    plugins: [new HtmlWebpackPlugin({template: "./src/index.html"})],
+}
