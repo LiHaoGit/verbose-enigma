4d6f4ed4518e260120c7419ef0cf6432303cad8b
lihao <dahao@qq.com>
2021-05-20 11:40:07 +0800

    实现自己的createElement与createTextElement

--- src/index.js
+++ src/index.js
@@ -1,1 +1,66 @@
-console.log("start")
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) => {
+        return typeof child === "object" ? child : createTextElement(child);
+      }),
+    },
+  };
+}
+/**@jsx createElement */
+const element = (
+  <div>
+    <h1>Hello World</h1>
+    <h2>from Didact</h2>
+  </div>
+);
+console.log(JSON.stringify(element,null,2))
+/**
+{
+  "type": "div",
+  "props": {
+    "children": [
+      {
+        "type": "h1",
+        "props": {
+          "children": [
+            {
+              "type": "TEXT_ELEMENT",
+              "props": {
+                "nodeValue": "Hello World",
+                "children": []
+              }
+            }
+          ]
+        }
+      },
+      {
+        "type": "h2",
+        "props": {
+          "children": [
+            {
+              "type": "TEXT_ELEMENT",
+              "props": {
+                "nodeValue": "from Didact",
+                "children": []
+              }
+            }
+          ]
+        }
+      }
+    ]
+  }
+}
+ */
